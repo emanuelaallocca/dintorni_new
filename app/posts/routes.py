7ea6635662ebe5c46d78779071a50e701c828b11
emanuelaallocca <emanuelaallocca@yahoo.it>
2022-03-02 16:07:42 +0100

    update

--- app/posts/routes.py
+++ app/posts/routes.py
@@ -1,7 +1,9 @@
+from datetime import datetime
+
 from flask import render_template, url_for, flash, redirect, request, abort
 from app import db
 from app.posts.forms import PostForm
-from models import Post
+from models import Post, JoinEvent, Event
 from flask_login import current_user, login_required
 from app.posts import posts
 from app.events import events
@@ -14,13 +16,21 @@ from app.events import events
 @login_required
 def new_post():
     form = PostForm()
+    page = request.args.get('page', 1, type=int)
+    jevents = JoinEvent.query.filter_by(user_id=current_user.id).all()
+    events_already_done = []
+    for e in jevents:
+        ev = Event.query.filter_by(id=e.event_id).first()
+        if ev:
+            if ev.date_event < datetime.today().date():
+                events_already_done.append(ev)
     if form.validate_on_submit():
         post = Post(title=form.title.data, content=form.content.data, author=current_user)
         db.session.add(post)
         db.session.commit()
         flash('post created', 'success')
         return redirect(url_for('main.home'))
-    return render_template('create_post.html', title='New Post', form=form, legend='New Post')
+    return render_template('create_post.html', title='New Post', form=form, legend='New Post', events = events)
 
 @posts.route("/post/<int:post_id>", methods=['GET', 'POST'])
 @login_required
@@ -60,5 +70,10 @@ def delete_post(post_id):
     flash('Your post has been deleted!', 'success')
     return redirect(url_for('main.home'))
 
+@posts.route("/reviews", methods=['GET', 'POST'])
+def reviews():
+    page = request.args.get('page', 1, type=int)  # possiamo passare il numero di post che vogliamo per pagina
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5) #richiamo tutti i post
+    return render_template('comments.html', posts=posts)
 
 
