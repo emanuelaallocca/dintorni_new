1bf17c1e9e592fc5278c47e70e819da12a59d37c
chiaramartucci <martucci.chia@gmail.com>
2022-04-03 15:07:11 +0200

    a caso 2

--- app/main/routes.py
+++ app/main/routes.py
@@ -6,7 +6,7 @@ from flask import render_template, url_for, redirect, request
 from app import db, bcrypt
 from models import Post, Event, Business, User, JoinEvent, Private
 from app.main import main
-#from PIL import Ima #non so perche non vada min 38.18 lezione 7
+#from PIL import Ima #non so pdwcderche non vada min 38.18 lezione 7
 import app
 
 
